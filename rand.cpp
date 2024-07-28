bbceced9350f1ab2d28e3769fa4a29b915dd0737
Janek <taw3e8@gmail.com>
2024-07-28 15:13:06 +0200

    rand cpp & hpp

--- rand.cpp
+++ rand.cpp
@@ -1,41 +1,74 @@
+#include <cassert>
+
 #include "rand.hpp"
 
-#include <iostream>
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
-    // Random r(argc != 1 ? std::stoi(argv[1]) : 137);
-    Random r(Random::trueRandomSeed_t{});
 
-    for(int i = 0; i < 10; ++i)
-        std::cout << r.intFromRange(0, 4) << ' ';
-    std::cout << '\n';
-    for(auto&& it : r.intsFromRange(10, 3, 6))
-        std::cout << it<< ' ';
-    std::cout << '\n';
-    for(auto&& it : r.intsFromRange(10, 10))
-        std::cout << it<< ' ';
-    std::cout << '\n';
+using IntType = Random::IntType;
 
-    for(int i = 0; i < 10; ++i)
-        std::cout << r.doubleFromRange(0, 4) << ' ';
-    std::cout << '\n';
-    for(auto&& it : r.doublesFromRange(10, 3, 6))
-        std::cout << it<< ' ';
-    std::cout << '\n';
-    for(auto&& it : r.doublesFromRange(10, 10))
-        std::cout << it<< ' ';
-    std::cout << '\n';
+IntType Random::intFromRange(IntType a, IntType b) {
+    assert(a < b);
+    std::uniform_int_distribution<IntType> dist(a, b - 1);
+    return dist(engine);
+}
 
-    for(int i = 0; i < 10; ++i)
-        std::cout << r.doubleFrom01() << ' ';
-    std::cout << '\n';
-    for(auto&& it : r.doublesFrom01(10))
-        std::cout << it<< ' ';
-    std::cout << '\n';
+std::vector<IntType> Random::intsFromRange(std::size_t n, IntType a, IntType b) {
+    assert(a < b);
+    std::vector<IntType> ret(n);
+    std::uniform_int_distribution<IntType> dist(a, b - 1);
+    while(n--) {
+        ret[n] = dist(engine);
+    }
+    return ret;
+}
+
+double Random::doubleFromRange(double a, double b) {
+    assert(std::isfinite(a) && std::isfinite(b) && a < b);
+    std::uniform_real_distribution dist(a, b);
+    return dist(engine);
+}
+
+std::vector<double> Random::doublesFromRange(std::size_t n, double a, double b) {
+    assert(std::isfinite(a) && std::isfinite(b) && a < b);
+    std::vector<double> ret(n);
+    std::uniform_real_distribution dist(a, b);
+    while(n--) {
+        ret[n] = dist(engine);
+    }
+    return ret;
+}
+
+std::vector<IntType> Random::perm(std::size_t n, IntType a) noexcept {
+    std::vector<IntType> ret(n);
+    std::iota(std::begin(ret), std::end(ret), IntType{a});
+    std::shuffle(std::begin(ret), std::end(ret), engine);
+    return ret;
+}
+
+std::vector<IntType> Random::distinct(std::size_t n, IntType a, IntType b) noexcept {
+    std::vector<IntType> ret;
+    ret.reserve(n);
+
+    std::vector<IntType> range(b - a);
+    std::iota(std::begin(range), std::end(range), a);
+    std::ranges::sample(range, std::back_inserter(ret), n, engine);
+
+    return ret;
+}
+
+std::vector<IntType> Random::partition(std::size_t n, IntType sum, IntType min) noexcept {
+    assert(sum >= n * min);
+    IntType adjusted_sum = sum - n * min;
+
+    std::vector<IntType> points = intsFromRange(n-1, adjusted_sum);
+    points.emplace_back(IntType{0});
+    points.emplace_back(adjusted_sum);
     
-    for(auto&& it : r.perm(10))
-        std::cout << it<< ' ';
-    std::cout << '\n';
-    
-    
+    std::sort(std::begin(points), std::end(points));
 
+    std::vector<IntType> partitions(n);
+    for(std::size_t i = 0; i < n; ++i) {
+        partitions[i] = points[i + 1] - points[i] + min;
+    }
+
+    return shuffle(partitions);
 }
--- rand.hpp
+++ rand.hpp
@@ -1,113 +1,87 @@
+#pragma once
+#ifndef OLYMPIC_MINDS_TESTFRAME_RAND_HPP
+#define OLYMPIC_MINDS_TESTFRAME_RAND_HPP
+
+
 #include <random>
 #include <cstdint>
-#include <cassert>
-#include <vector>
-#include <algorithm>
-#include <ranges>
 #include <algorithm>
 
-// random_shuffle z zakresu
-// partition
-// shuffle
 
-class Random {
+struct [[nodiscard]] Random {
 public:
     using IntType = std::uint64_t;
-    class trueRandomSeed_t {};
 
-    IntType seed;    
-    std::mt19937_64 engine;
+    IntType seed{};    
+    std::mt19937_64 engine{};
 
 public:
-    Random(IntType seed = 137)
+    explicit Random(IntType seed) noexcept
         : seed(seed), engine(seed)
-    {}
-
-    // seeds with random device
-    Random(trueRandomSeed_t)
-        : seed(std::random_device{}()), engine(seed)
     {}
 
 public:
     // one uniform IntType from interval [a, b)
-    IntType intFromRange(IntType a, IntType b) {
-        assert(a < b);
-        std::uniform_int_distribution<IntType> dist(a, b - 1);
-        return dist(engine);
-    }
+    [[nodiscard]] IntType intFromRange(IntType a, IntType b);
     // one uniform IntType from interval [0, b)
-    IntType intFromRange(IntType b) {
+    [[nodiscard]] inline IntType intFromRange(IntType b) {
         return intFromRange(0, b);
     }
 
     // n uniform ints from interval [a, b)
-    std::vector<IntType> intsFromRange(std::size_t n, IntType a, IntType b) {
-        assert(a < b);
-        std::vector<IntType> ret(n);
-        std::uniform_int_distribution<IntType> dist(a, b - 1);
-        while(n--) {
-            ret[n] = dist(engine);
-        }
-        return ret;
-    }
+    [[nodiscard]] std::vector<IntType> intsFromRange(std::size_t n, IntType a, IntType b);
     // n uniform ints from interval [0, b)
-    std::vector<IntType> intsFromRange(std::size_t n, IntType b) {
-        assert(0 < b);
+    [[nodiscard]] inline std::vector<IntType> intsFromRange(std::size_t n, IntType b) {
         return intsFromRange(n, 0, b);
     }
 
     // one uniform double from interval [a, b)
-    double doubleFromRange(double a, double b) {
-        assert(std::isfinite(a) && std::isfinite(b) && a < b);
-        std::uniform_real_distribution dist(a, b);
-        return dist(engine);
-    }
+    [[nodiscard]] double doubleFromRange(double a, double b);
     // one uniform double from interval [0.0, b)
-    double doubleFromRange(double b) {
+    [[nodiscard]] inline double doubleFromRange(double b) {
         return doubleFromRange(0., b);
     }
     // one uniform double from interval [0.0, 1.0)
-    double doubleFrom01() {
+    [[nodiscard]] inline double doubleFrom01() {
         return doubleFromRange(0., 1.);
     }
 
     // n uniform doubles from interval [a, b)
-    std::vector<double> doublesFromRange(std::size_t n, double a, double b) {
-        assert(std::isfinite(a) && std::isfinite(b) && a < b);
-        std::vector<double> ret(n);
-        std::uniform_real_distribution dist(a, b);
-        while(n--) {
-            ret[n] = dist(engine);
-        }
-        return ret;
-    }
+    [[nodiscard]] std::vector<double> doublesFromRange(std::size_t n, double a, double b);
     // n uniform doubles from interval [0.0, b)
-    std::vector<double> doublesFromRange(std::size_t n, double b) {
+    [[nodiscard]] inline std::vector<double> doublesFromRange(std::size_t n, double b) {
         return doublesFromRange(n, 0.0, b);
     }
     // n uniform doubles from interval [0.0, 1.0)
-    std::vector<double> doublesFrom01(std::size_t n) {
+    [[nodiscard]] inline std::vector<double> doublesFrom01(std::size_t n) {
         return doublesFromRange(n, 0.0, 1.0);
     }
 
-    // shuffled permutation of [0, n)
-    std::vector<IntType> perm(std::size_t n) {
-        return perm(n, IntType{0});
-    }
-    // shuffled permutation of [from, from+n)
-    std::vector<IntType> perm(std::size_t n, IntType from) {
-        std::vector<IntType> ret(n);
-        std::iota(std::begin(ret), std::end(ret), IntType{from});
-        std::shuffle(std::begin(ret), std::end(ret), engine);
-        return ret;
-    }
+    // shuffled permutation of [a, a+n)
+    [[nodiscard]] std::vector<IntType> perm(std::size_t n, IntType a = 0) noexcept;
 
     // shuffle a range
     template<typename RandomIt>
-    void shuffle(RandomIt first, RandomIt last) {
+    inline void shuffle(RandomIt first, RandomIt last) {
         std::shuffle(first, last, engine);
     }
+    // shuffle a range
+    template<class R>
+    [[nodiscard]] inline R shuffle(R&& range) {
+        std::shuffle(std::begin(range), std::end(range), engine);
+        return range;
+    }
 
+    // selects min(n, b-a) distinct numbers from [a, b); stable
+    [[nodiscard]] std::vector<IntType> distinct(std::size_t n, IntType a, IntType b) noexcept;
+    // selects min(n, b) distinct numbers from [0, b); stable
+    [[nodiscard]] inline std::vector<IntType> distinct(std::size_t n, IntType b) noexcept {
+        return distinct(n, IntType{0}, b);
+    }
 
-
+    // splits sum into n parts such that each of them is at least min
+    [[nodiscard]] std::vector<IntType> partition(std::size_t n, IntType sum, IntType min = 1) noexcept;
 };
+
+
+#endif // OLYMPIC_MINDS_TESTFRAME_RAND_HPP

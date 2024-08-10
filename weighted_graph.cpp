c8af2814579cc5cb20cab458fb5d03cc5a569ad6
Atanazyy <atanazy.gawrysiak@gmail.com>
2024-08-10 16:54:49 +0200

    fix addRandomWeights

--- weighted_graph.cpp
+++ weighted_graph.cpp
@@ -9,9 +9,11 @@
 WeightedGraph WeightedGraph::addRandomWeights(Graph g, std::int64_t w_min, std::int64_t w_max) {
     std::vector<std::vector<std::pair<std::uint64_t, std::int64_t>>> graph(g.getNumberOfNodes());
     for (auto [u, v] : g.getEdges()) {
-        std::int64_t w = rnd.intFromRange(w_min, w_max);
-        graph[u].push_back({v, w});
-        graph[v].push_back({u, w});
+        if(u <= v) {
+            std::int64_t w = rnd.intFromRange(w_min, w_max);
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, w});
+        }
     }
     return WeightedGraph(graph);
 }
